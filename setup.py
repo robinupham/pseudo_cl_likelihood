0a81a821bd315fee9d402b5de9b3977227195796
Robin Upham <rupham1@gmail.com>
2021-08-13 11:00:31 +0100

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='pseudo_cl_likelihood',
+      version='1.0',
+      description='Pseudo-Cl likelihood',
+      author='Robin Upham',
+      url='https://github.com/robinupham/pseudo_cl_likelihood',
+      packages=find_packages())
