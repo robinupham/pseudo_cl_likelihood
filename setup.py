d1bcc07e76ebde2612faa1f06f777f85bb18bf9a
Robin Upham <rupham1@gmail.com>
2021-08-13 11:19:04 +0100

    explicitly specify package name

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='pseudo_cl_likelihood',
       version='1.0',
       description='Pseudo-Cl likelihood',
       author='Robin Upham',
       url='https://github.com/robinupham/pseudo_cl_likelihood',
-      packages=find_packages(),
+      packages=['pseudo_cl_likelihood'],
       install_requires=['numpy', 'healpy', 'pyshtools'],)
